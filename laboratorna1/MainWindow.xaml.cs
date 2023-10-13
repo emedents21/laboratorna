ebd318286fad68facc4482996c7b03a55991197d
Yehor Medentsov <Yehor Medentsov@DESKTOP-BJQG4A5>
2023-10-12 20:13:40 -0700

    Добавьте файлы проекта.

---
+++ laboratorna1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laboratorna1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Tree tr;
+
+            string nname = name.Text;
+            int aage;
+            double hheight;
+
+            if (int.TryParse(age.Text, out aage) && double.TryParse(height.Text, out hheight))
+            {
+                tr = new Tree(nname, hheight, aage);
+                double averageGrowth = tr.CalculateAverageGrowthPerYear();
+                result.Text = $"Середній приріст деревини в рік: {averageGrowth} см/рок";
+            }
+            else
+            {
+                result.Text = "Помилка: Невірний формат введених даних.";
+            }
+        }
+    }
+}
---
+++ laboratorna1/Tree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorna1
+{
+    internal class Tree
+    {
+        // Поля класу
+        string name;
+        double height;
+        int age;
+
+        public Tree(string name, double height, int age)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Назва дерева не може бути порожньою або null.", nameof(name));
+            }
+            if (height < 0)
+            {
+                throw new ArgumentException("Висота дерева не може бути від'ємною.", nameof(height));
+            }
+            if (age < 0)
+            {
+                throw new ArgumentException("Вік дерева не може бути від'ємним.", nameof(age));
+            }
+
+            this.name = name;
+            this.height = height;
+            this.age = age;
+        }
+        public double CalculateAverageGrowthPerYear()
+        {
+            if (age == 0)
+            {
+                return 0.0;
+            }
+
+            return height / age;
+        }
+
+     
+    }
+}
+
